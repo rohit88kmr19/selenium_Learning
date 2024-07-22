edcaec0570fdba24d87a0b62ae3dfdecc7162439
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-22 12:19:10 +0530

    Fibbonaci Series Programme Added.

--- src/JavaProgrammes/FibbSeries.java
+++ src/JavaProgrammes/FibbSeries.java
@@ -2,6 +2,25 @@ package JavaProgrammes;
 
 public class FibbSeries {
     public static void main(String[] args) {
+        fibCal();
+    }
+    public static void fibCal()
+    {
+        int n1=0;
+        int n2=1;
+
+        System.out.println(n1 + " " +n2);
+
+        for (int i=0; i<=4;i++)
+        {
+            int n3;
+            n3=n1+n2;
+            System.out.println(n3);
+            n1=n2;
+            n2=n3;
+
+        }
 
     }
+
 }
