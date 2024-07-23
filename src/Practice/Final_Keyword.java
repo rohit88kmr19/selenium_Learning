200c5cae39628415f2d6ff9a417517cab5e6a961
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-23 18:14:35 +0530

    final Keyword and escape characters.

--- src/Practice/Final_Keyword.java
+++ src/Practice/Final_Keyword.java
@@ -2,11 +2,26 @@ package Practice;
 
 public class Final_Keyword {
     public static void main(String[] args) {
-        final int A=45;
-       // int A=67;
+        final int A = 45;
+        // int A=67;
 
-        final double PIE =3.14;
+        final double PIE = 3.14;
         System.out.println(A);
         System.out.println(PIE);
+
+        char c = '\n';
+        System.out.println(c);
+
+        char c1 = '\t';
+        char c2 = '\b';
+        char c3 = '\r';
+
+        System.out.println("Rohit\n"+ "Kumar"); // New Line
+        System.out.println("rohit" +c1+ "Kumar"); //Tab between words
+        System.out.println("rohit" +c2+ "Kumar");//backlash, deletes 1 char
+        System.out.println("rohit" +c3+ "Kumar");// backlash and deletes 1 word
+
+        // These are called escape characters.
+    }
 
 }
