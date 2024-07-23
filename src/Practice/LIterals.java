7be7819afd2091cbb09b7b5674f7b2dbfcaece98
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-23 18:27:33 +0530

    Literals and operators file added.

---
+++ src/Practice/LIterals.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class LIterals {
+    public static void main(String[] args) {
+         int a = "rohit";
+
+         boolean b= true;
+
+         String str = null;
+        System.out.println(str);
+
+        // Literal should be same as per the data type used.
+
+
+
+    }
+}
---
+++ src/Practice/Operators.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class Operators {
+    public static void main(String[] args) {
+        int age = 65;// 65 is literal and age is reference variable and int is data type and = is the assignment operator.
+
+        int a=10;
+        int b=20;
+        System.out.println(a+b);//Arithematic operators like +, - , / , * , %
+
+        int type = +98;
+        int lambo =-1;
+        System.out.println(type);//+ is unary
+        System.out.println(lambo);// -1 is unary.
+
+
+
+    }
+}
+
