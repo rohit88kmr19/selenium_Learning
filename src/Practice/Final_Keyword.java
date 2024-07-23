6828fa66f0becf37e6fa71e4174cc8e34c5bb995
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-23 18:05:49 +0530

    final Keyword

---
+++ src/Practice/Final_Keyword.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Final_Keyword {
+    public static void main(String[] args) {
+        final int A=45;
+       // int A=67;
+
+        final double PIE =3.14;
+        System.out.println(A);
+        System.out.println(PIE);
+
+}
