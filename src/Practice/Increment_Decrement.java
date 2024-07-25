c8b9b6842364f4b4a1812b56f8cd82f68ebb18e9
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-25 13:47:46 +0530

    Increment_DecremenT added.

--- src/Practice/Increment_Decrement.java
+++ src/Practice/Increment_Decrement.java
@@ -3,35 +3,37 @@ package Practice;
 public class Increment_Decrement {
     public static void main(String[] args) {
 
-        //Use LAB60 for this.
-
-        // Increment (++) / Decrement (--) Operators
-        // Pre and Post
-        // Ready
-
-//         pre - increment ++operand
-//         value is incremented first and then stored in the result.
-        int a = 10;
-        int b = ++a; // a = a+1;
-        // Exp = 11 , a = 11
-        System.out.println(a);
-        System.out.println(b);
-
-
+//        //Use LAB60 for this.
 //
-        int a = 10;
-        System.out.println(++a);
-        // Exp = 11 , a = 11
+//        // Increment (++) / Decrement (--) Operators
+//        // Pre and Post
+//        // Ready
+//
+////         pre - increment ++operand
+////         value is incremented first and then stored in the result.
+//        int a = 10;
+//        int b = ++a; // a = a+1;
+//        // Exp = 11 , a = 11
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//
+////
+//        int a = 10;
+//        System.out.println(++a);
+//        // Exp = 11 , a = 11
+//
+//
+//        // POST
+//        //  value is stored in the result and incremented later
+//        int a_post = 10;
+//        System.out.println(a_post++);
+//        System.out.println(a_post);
+//        // Exp = 10 , a = 11
 
-
-        // POST
-        //  value is stored in the result and incremented later
-        int a_post = 10;
-        System.out.println(a_post++);
-        System.out.println(a_post);
-        // Exp = 10 , a = 11
-
-
+            int t=10;
+        System.out.println(++t + t++ + t++); // 11+11+12=34
+        System.out.println(t);//13
 
 
     }
