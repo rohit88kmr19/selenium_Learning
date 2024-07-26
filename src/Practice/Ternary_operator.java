7a495773a553f7d7904cb00c161f4e302a903dfc
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-26 18:16:09 +0530

    Ternary added.

---
+++ src/Practice/Ternary_operator.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class Ternary_operator {
+    public static void main(String[] args) {
+
+        boolean result = 10>40 ? false: true;
+        System.out.println(result);
+    }
+}
