a3fa52fe7dab93c04e66634cc32e7272c6e066de
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-27 00:22:04 +0530

    New Test

--- src/Practice/GitAddfile.java
+++
@@ -1,7 +0,0 @@
-package Practice;
-
-public class GitAddfile {
-    public static void main(String[] args) {
-        System.out.println("hello my friend");
-    }
-}
--- src/Practice/Test.java
+++ src/Practice/Test.java
@@ -2,10 +2,6 @@ package Practice;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
-
-        System.out.println("testj,dfjdkbsfjkfh");
-
 
     }
 }
