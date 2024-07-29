6ba8d805e23c755b70b304228242375e8a91b630
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-07-29 22:06:25 +0530

    odd even added

---
+++ src/JavaProgrammes/Odd_Even.java
@@ -0,0 +1,14 @@
+package JavaProgrammes;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+        int num =11;
+        if(num%2==0)
+        {
+            System.out.println("Even");
+        }
+            else {
+            System.out.println("Odd");
+        }
+    }
+}
