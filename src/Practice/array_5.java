512f31efc69ac5998fde972cfe5b20d2c5528e79
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-08-03 23:46:53 +0530

    array new file added.

---
+++ src/Practice/array_5.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class array_5 {
+    public static void main(String[] args) {
+
+        int [] ar1= {32, 45,78};
+        long [] ar2= {32l  , 45l,78l};
+        float [] ar3= {32f, 45f,78f};
+        double [] ar4= {32.33, 45.66,78.99};
+        char [] ar45= {'A', 'B','C'};
+        String [] ar46= {"Rohit", "Hello"};
+
+        int ages[]= new int[2];
+        int [] ages1 = new int[3];
+
+
+    }
+}
