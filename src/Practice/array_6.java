3b085f39b64e735c7e002d87f43bc3a9315b9d9d
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-08-05 16:52:35 +0530

    array new file added.

--- src/Practice/array_6.java
+++ src/Practice/array_6.java
@@ -1,10 +1,21 @@
 package Practice;
 
+import javax.swing.*;
+
 public class array_6 {
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
+//        System.out.println(args[0]);
+//        System.out.println(args[1]);
+//        System.out.println(args[2]);
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
 
-    }
-}
+                System.out.println(args[i]);
+            }
+        }
+            else{
+                System.out.println("No arguments");
+            }
+            }
+        }
+
