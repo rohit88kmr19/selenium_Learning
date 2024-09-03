83b7aa1b30e52bab860c6efee02ada34b04b3a35
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-09-03 18:45:20 +0530

    Collection Framework and Exception

--- src/Oops_programme_cpncepts/CollectionFramewo/CF7_lis.java
+++ src/Oops_programme_cpncepts/CollectionFramewo/CF7_lis.java
@@ -1,6 +1,8 @@
 package Oops_programme_cpncepts.CollectionFramewo;
 
 import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.Vector;
 
 public class CF7_lis {
@@ -25,6 +27,27 @@ public class CF7_lis {
 
         String s1 = (String) v.get(1);
         System.out.println(s1);
+
+        Stack s = new Stack();
+
+        s.push("rphit");
+        s.push("lohoit");
+        s.push("mohit");
+        System.out.println(s);
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s);
+
+        PriorityQueue pq= new PriorityQueue();
+        pq.offer("1");
+        pq.offer("2");
+        pq.offer("3");
+        pq.offer("4");
+
+        System.out.println(pq);
+        System.out.println(pq.peek());
+        System.out.println(pq.poll());
+        System.out.println(pq);;
 
     }
 }
